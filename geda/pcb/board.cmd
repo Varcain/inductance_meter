fe05a6e8db3bdeb344bf7164f2ecca76e6ff2469
Kamil Lulko <rev13@wp.pl>
2015-09-30 23:41:38 +0200

    Update schematic, add pcb layout

---
+++ geda/pcb/board.cmd
@@ -0,0 +1,53 @@
+# Pin name action command file
+
+# Start of element CONN2
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 4, -Ub)
+ChangePinName(U1, 8, +Ub)
+ChangePinName(U1, 6, balance)
+ChangePinName(U1, 5, balance)
+ChangePinName(U1, 1, GND)
+ChangePinName(U1, 7, Vout)
+ChangePinName(U1, 2, +)
+ChangePinName(U1, 3, -)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
